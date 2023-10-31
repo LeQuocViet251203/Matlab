ba2fc210683414b2d604b13e04a6e47e791f7e7b
Le Quoc Viet <110276410+LeQuocViet251203@users.noreply.github.com>
2023-11-01 04:25:01 +0700

    Working with command window and Script file

---
+++ Script1.m
@@ -0,0 +1,4 @@
+f = input('Nhap vao tan so f(Hz):') ; %yeu cau nguoi dung nhap vao tan so
+C= input('Nhap vao dien dung C(F):')  ; %yeu cau nguoi dung nhap vao dien dung
+disp('Dung khang la :') ; 
+Zc = 1 / ( 2 * pi * f * C ) ;
