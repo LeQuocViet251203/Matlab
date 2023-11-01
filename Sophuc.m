a8dc05fcc9344a4d317a475020ed18d9ad897d52
Le Quoc Viet <110276410+LeQuocViet251203@users.noreply.github.com>
2023-11-01 16:14:42 +0700

    Complex number , Trigonometric , Matrix

---
+++ Sophuc.m
@@ -0,0 +1,12 @@
+z = input('Nhap vao so phuc z: ') ; 
+disp('Phan thuc: ' ) ;
+Phanthuc = real(z) 
+disp('Phan ao: '  ) ;
+Phanao = imag(z) ; 
+disp('Modun: '  ) ; 
+Modun = abs(z) ; 
+disp('Lien hop phuc: '  ) ; 
+LHP = conj(z) ; 
+disp('argumen: ') ; 
+argumen = angle(z) ; 
+
